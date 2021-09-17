2860c3f6f951d0595b41aba48e182545c536a856
Janekdererste <pewee-rescuer-delicacy-mum-pariah-thespian-weird-buzzy-dolby-tremor-shallow-spent-jag-geo-del>
2021-09-17 16:05:36 +0200

    Add unit test for bicylce routing and scoring regarind the elevation of the network

--- src/test/java/org/matsim/run/TestBicycleRouting.java
+++ src/test/java/org/matsim/run/TestBicycleRouting.java
@@ -6,27 +6,37 @@ import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.application.MATSimApplication;
+import org.matsim.contrib.bicycle.BicycleConfigGroup;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.population.io.PopulationReader;
+import org.matsim.core.population.routes.NetworkRoute;
+import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.testcases.MatsimTestUtils;
+import picocli.CommandLine;
+
+import java.nio.file.Path;
+
+import static org.junit.Assert.*;
 
 public class TestBicycleRouting {
+
+    private static Id<Person> personId = Id.createPersonId("test-person");
 
     @Rule
     public MatsimTestUtils testUtils = new MatsimTestUtils();
 
     @Test
-    public void testElevationRouting() throws Exception {
+    public void testElevationRouting() {
 
         var outputDir = testUtils.getOutputDirectory();
 
-        var appWithElevation = new TestApplication(true, outputDir + "withElevation");
-        appWithElevation.call();
-
-        var appWithoutElevation = new TestApplication(false, outputDir + "withoutElevation");
-        appWithoutElevation.call();
+        MATSimApplication.execute(TestApplication.class, "run", "--output=" + outputDir + "withElevation", "--useElevation=true");
+        MATSimApplication.execute(TestApplication.class, "run", "--output=" + outputDir + "withoutElevation", "--useElevation=false");
 
         // load output of both runs
         var scenarioWithElevation = ScenarioUtils.createScenario(ConfigUtils.createConfig());
@@ -39,30 +49,44 @@ public class TestBicycleRouting {
         // somehow compare the two routes
         System.out.println("bla test");
 
+        var personWithElevation = scenarioWithElevation.getPopulation().getPersons().get(personId);
+        var personWithoutElevation = scenarioWithoutElevation.getPopulation().getPersons().get(personId);
 
+        assertTrue(personWithElevation.getSelectedPlan().getScore() < personWithoutElevation.getSelectedPlan().getScore());
 
+        var bikeRouteWithElevation = getBikeRoute(personWithElevation);
+        var bikeRouteWithoutElevation = getBikeRoute(personWithoutElevation);
 
+        assertNotEquals(bikeRouteWithElevation.toString(), bikeRouteWithoutElevation.toString());
     }
 
-    private static class TestApplication extends RunMetropoleRuhrScenario {
+    private static NetworkRoute getBikeRoute(Person person) {
+        return person.getSelectedPlan().getPlanElements().stream()
+                .filter(element -> element instanceof Leg)
+                .map(element -> (Leg)element)
+                .filter(leg -> leg.getMode().equals(TransportMode.bike))
+                .map(bikeLeg -> (NetworkRoute) bikeLeg.getRoute())
+                .findAny()
+                .orElseThrow();
+    }
+
+    public static class TestApplication extends RunMetropoleRuhrScenario {
+
+        @CommandLine.Option(names = "--useElevation", description = "Overwrite output folder defined by the application")
+        protected boolean isUseElevation;
 
         private static final String RUN_ID = "TestApplication";
-        private final boolean useElevation;
-        private final String outputFolder;
-
-        TestApplication(boolean useElevation, String outputFolder) {
-            super(ConfigUtils.loadConfig("./scenarios/metropole-ruhr-v1.0/input/metropole-ruhr-v1.0-10pct.config.xml"));
-            this.useElevation = useElevation;
-            this.outputFolder = outputFolder;
-        }
 
         @Override
         public Config prepareConfig(Config config) {
             var preparedConfig = super.prepareConfig(config);
             preparedConfig.plans().setInputFile(null);
-            preparedConfig.controler().setOutputDirectory(outputFolder);
-            preparedConfig.controler().setLastIteration(1);
+            preparedConfig.controler().setLastIteration(0);
             preparedConfig.controler().setRunId(RUN_ID);
+
+            var bikeConfig = ((BicycleConfigGroup) config.getModules().get("bicycle"));
+            // set an insanely high disutility for gradients
+            bikeConfig.setMarginalUtilityOfGradient_m_100m(-1000);
             return preparedConfig;
         }
 
@@ -84,12 +108,12 @@ public class TestBicycleRouting {
             var other = factory.createActivityFromCoord("other_3600.0",otherCoord);
             other.setEndTime(3600);
             plan.addActivity(other);
-            var person = factory.createPerson(Id.createPersonId("test-person"));
+            var person = factory.createPerson(personId);
             person.addPlan(plan);
             scenario.getPopulation().addPerson(person);
 
             // remove elevation if necessary
-            if (!useElevation) {
+            if (!isUseElevation) {
                 scenario.getNetwork().getNodes().values().parallelStream()
                         .forEach(node -> {
                             // copy coord but ignore z-component
