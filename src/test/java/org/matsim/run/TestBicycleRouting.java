459313ee9af5f4079d2d7ffacc352997c6857885
Janekdererste <janek.laudan@mailbox.org>
2021-11-30 12:42:35 +0100

    Remove local file path

--- src/test/java/org/matsim/run/TestBicycleRouting.java
+++ src/test/java/org/matsim/run/TestBicycleRouting.java
@@ -8,7 +8,6 @@ import org.locationtech.jts.geom.prep.PreparedGeometry;
 import org.locationtech.jts.geom.prep.PreparedGeometryFactory;
 import org.matsim.api.core.v01.*;
 import org.matsim.api.core.v01.network.Network;
-import org.matsim.api.core.v01.network.NetworkWriter;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.application.MATSimApplication;
@@ -30,7 +29,6 @@ import static org.junit.Assert.assertTrue;
 public class TestBicycleRouting {
 
     private static final Id<Person> personId = Id.createPersonId("test-person");
-    //private static final String inputNetworkFile = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v1.0/test-input/metropole-ruhr-v1.0.network_resolutionHigh-with-pt-bicylceTest.xml.gz";
     private static final String inputNetworkFile = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v1.0/input/metropole-ruhr-v1.0.network_resolutionHigh-with-pt.xml.gz";
 
     @Rule
@@ -135,8 +133,6 @@ public class TestBicycleRouting {
             for (var id : nodeIdsToRemove) {
                 scenario.getNetwork().removeNode(id);
             }
-
-            new NetworkWriter(scenario.getNetwork()).write("C:/Users/Janekdererste/Desktop/reduced-network.xml.gz");
 
             // remove elevation if necessary
             if (!isUseElevation) {
