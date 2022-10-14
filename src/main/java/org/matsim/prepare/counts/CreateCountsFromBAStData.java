221218c035ea0880c0fc49180ce07fe0731e1c33
hzoerner <h.zoerner@campus.tu-berlin.de>
2022-10-14 14:46:38 +0200

    implemented network filter for mode, link type and id added shp and crs options

--- src/main/java/org/matsim/prepare/counts/CreateCountsFromBAStData.java
+++ src/main/java/org/matsim/prepare/counts/CreateCountsFromBAStData.java
@@ -6,11 +6,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.application.MATSimAppCommand;
+import org.matsim.application.options.CrsOptions;
+import org.matsim.application.options.ShpOptions;
+import org.matsim.core.config.groups.NetworkConfigGroup;
 import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.network.filter.NetworkFilterManager;
 import org.matsim.counts.Counts;
+import org.matsim.counts.CountsWriter;
 import picocli.CommandLine;
 
 import java.io.FileNotFoundException;
@@ -18,6 +24,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @CommandLine.Command(name = "counts-from-bast", description = "creates MATSim from BASt Stundenwerte.txt")
 public class CreateCountsFromBAStData implements MATSimAppCommand {
@@ -34,6 +42,15 @@ public class CreateCountsFromBAStData implements MATSimAppCommand {
     @CommandLine.Option(names = "--isMotorwayData", description = "Boolean, if BASt data contains motorways or Bundesstraßen", required = true)
     private boolean isMotorwayData;
 
+    @CommandLine.Option(names = "--output", description = "output counts path", defaultValue = "counts-from-bast.xml.gz")
+    private String output;
+
+    @CommandLine.Mixin
+    private ShpOptions shp = new ShpOptions();
+
+    @CommandLine.Mixin
+    private CrsOptions crs = new CrsOptions();
+
     private static final Logger log = LogManager.getLogger(CreateCountsFromBAStData.class);
 
     public static void main(String[] args) {
@@ -41,7 +58,7 @@ public class CreateCountsFromBAStData implements MATSimAppCommand {
     }
 
     @Override
-    public Integer call() throws Exception {
+    public Integer call() {
 
         /*
         * TODO
@@ -52,49 +69,66 @@ public class CreateCountsFromBAStData implements MATSimAppCommand {
         *  join hour-values on data set with coordinates
         * */
 
-        var stations = readBAStCountStations(aggregatedData);
+        var stations = readBAStCountStations(aggregatedData, shp, crs);
+
         matchBAStWithNetwork(network, stations);
 
         Counts counts = new Counts();
+        stations.stream()
+                .filter(station -> station.getMatchedLink() != null)
+                .forEach(station -> counts.createAndAddCount(station.getMatchedLink(), station.getName())
+                        .createVolume(1, 10000));
 
-        return null;
+        counts.setYear(2022);
+
+        new CountsWriter(counts).write(output);
+        return 0;
+    }
+
+    private void addRemovedLinksToNetwork(List<Link> removedLinks, Network network){
+        removedLinks.forEach(network::addLink);
     }
 
     private Id<Link> match(Network network, BAStCountStation station){
 
-        List<Link> removed = new ArrayList<>();
+        log.info("+++++++ Start matching for station " + station.getName() + " +++++++");
 
-        while (true){
+        var link = NetworkUtils.getNearestLink(network, station.getCoord());
 
-            var link = NetworkUtils.getNearestLink(network, station.coord);
-
-            if(link.getAttributes().getAttribute("type").equals("motorway") ||
-                    link.getAttributes().getAttribute("type").equals("primary")){
-
-                if(!removed.isEmpty()) removed.forEach(network::addLink);
-                return link.getId();
-            } else {
-
-                removed.add(link);
-                network.removeLink(link.getId());
-            }
+        if (link == null){
+            log.info("+++++++ Could not match station " + station.getName() + " +++++++");
+            return null;
         }
+
+        log.info("+++++++ Matched station " + station.getName() + " to link " + link.getId());
+        return link.getId();
     }
 
     private void matchBAStWithNetwork(String pathToNetwork, List<BAStCountStation> stations){
 
-        Network network = NetworkUtils.readNetwork(pathToNetwork);
+        Network filteredNetwork;
+
+        {
+            Network network = NetworkUtils.readNetwork(pathToNetwork);
+            NetworkFilterManager filter = new NetworkFilterManager(network, new NetworkConfigGroup());
+            filter.addLinkFilter(link -> link.getAllowedModes().contains(TransportMode.car));
+            filter.addLinkFilter(link -> "motorway".equals(link.getAttributes().getAttribute("type")) ||
+                    "primary".equals(link.getAttributes().getAttribute("type")) ||
+                    "trunk".equals(link.getAttributes().getAttribute("type")));
+            filter.addLinkFilter(link -> !link.getId().toString().startsWith("pt"));
+            filter.addLinkFilter(link -> !link.getId().toString().startsWith("bike"));
+
+            filteredNetwork = filter.applyFilters();
+        }
 
         for(var station: stations){
 
-            Id<Link> matchedId = match(network, station);
-            Link matchedLink = network.getLinks().get(matchedId);
-            Link oppDirLink = NetworkUtils.findLinkInOppositeDirection(matchedLink);
+            Id<Link> matchedId = match(filteredNetwork, station);
+            station.setMatchedLink(matchedId);
         }
-        return;
     }
 
-    private List<BAStCountStation> readBAStCountStations(String pathToAggregatedData){
+    private List<BAStCountStation> readBAStCountStations(String pathToAggregatedData, ShpOptions shp, CrsOptions crs){
 
         List<BAStCountStation> stations = new ArrayList<>();
 
@@ -109,6 +143,7 @@ public class CreateCountsFromBAStData implements MATSimAppCommand {
             for(var record: records){
 
                 String id = record.get("DZ_Nr");
+                String name = record.get("DZ_Name");
 
                 String dir1 = record.get("Hi_Ri1");
                 String dir2 = record.get("Hi_Ri2");
@@ -118,7 +153,7 @@ public class CreateCountsFromBAStData implements MATSimAppCommand {
 
                 Coord coord = new Coord(Double.parseDouble(x), Double.parseDouble(y));
 
-                BAStCountStation station = new BAStCountStation(id, dir1, dir2, coord);
+                BAStCountStation station = new BAStCountStation(id, name, dir1, dir2, coord);
                 stations.add(station);
             }
         } catch (FileNotFoundException e) {
@@ -128,14 +163,26 @@ public class CreateCountsFromBAStData implements MATSimAppCommand {
             e.printStackTrace();
         }
 
-        return stations;
+        final Predicate<BAStCountStation> filter;
+        if (shp.getShapeFile() != null) {
+            // default input is set to lat lon
+            ShpOptions.Index index = shp.createIndex(crs.getInputCRS(), "_");
+            filter = station -> index.contains(station.getCoord());
+        } else filter = (station) -> true;
+
+        return stations.stream()
+                .filter(filter)
+                .collect(Collectors.toList());
     }
 
     private static class BAStCountStation{
 
+        private final String name;
         private final String id;
         private final String dir1;
         private final String dir2;
+
+        private Id<Link> matchedLink;
 
         private final Coord coord;
 
@@ -144,12 +191,14 @@ public class CreateCountsFromBAStData implements MATSimAppCommand {
 
         private boolean hasDTV;
 
-        BAStCountStation(String id, String dir1, String dir2, Coord coord){
+        BAStCountStation(String id, String name, String dir1, String dir2, Coord coord){
 
             this.coord = coord;
             this.dir1 = dir1;
             this.dir2 = dir2;
             this.id = id;
+            this.name = name;
+
 
             this.hasDTV = false;
         }
@@ -180,6 +229,22 @@ public class CreateCountsFromBAStData implements MATSimAppCommand {
 
         public String getDir2() {
             return dir2;
+        }
+
+        public Coord getCoord() {
+            return coord;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Id<Link> getMatchedLink() {
+            return matchedLink;
+        }
+
+        public void setMatchedLink(Id<Link> matchedLink) {
+            this.matchedLink = matchedLink;
         }
     }
 }
