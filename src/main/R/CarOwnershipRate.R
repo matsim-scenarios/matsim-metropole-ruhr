cb588e8f0801e3970c69fa34729b1391660fad8d
GregorRyb <rybczak@vsp.tu-berlin.de>
2022-10-31 14:38:31 +0100

    change to CarOwnershipRate.R it works with shp file

--- src/main/R/CarOwnershipRate.R
+++ src/main/R/CarOwnershipRate.R
@@ -2,13 +2,24 @@ library(tidyverse)
 library(dplyr)
 library(patchwork)
 library(scales)
+library(sf)
 
 ### trips.csv to get car use
-completeTrips <- read_csv2("/Users/gregorr/Desktop/Test/Pkw_BEsitzraten/010.output_trips.csv.gz")
+completeTrips <- read_csv2("/Users/gregorr/Desktop/Test/Pkw_BEsitzraten/010.output_trips.csv.gz", col_types = cols(person = col_character()))
 ##
-persons <- read_csv2("/Users/gregorr/Desktop/Test/Pkw_BEsitzraten/010.output_persons.csv.gz")
+persons <- read_csv2("/Users/gregorr/Desktop/Test/Pkw_BEsitzraten/010.output_persons.csv.gz", col_types = cols(person = col_character()))
 ## person2homeRegionAndCoord
-person2region <-read_csv2("/Users/gregorr/Documents/work/respos/runs-svn/rvr-ruhrgebiet/v1.2.1/036/person2Home.csv")
+person2region <- read_csv("/Users/gregorr/Documents/work/respos/shared-svn/projects/matsim-metropole-ruhr/metropole-ruhr-v1.0/input/metropole-ruhr-v1.4-25pct.plans-homes.csv",
+                          col_types = cols(person = col_character(), home_x = col_double(), home_y = col_double()
+                          ))
+
+shape <- st_read("/Users/gregorr/Documents/work/respos/shared-svn/projects/rvr-metropole-ruhr/data/shapeFiles/dvg2krs_ruhrgebiet-rvr/dvg2krs_ruhrgebiet-rvr.shp")
+person2region <- person2region %>%
+  st_as_sf(coords = c("home_x", "home_y"), crs = 25832) %>%
+  st_join(shape)
+person2region <- select(person2region, person, geometry, GN)
+person2region <- person2region %>% replace(is.na(.), "other")
+
 ## joining person and persons2region
 persons <- left_join(persons, person2region, by= "person")
 
@@ -30,7 +41,7 @@ persons<-persons %>%
   mutate(usedCar = ifelse(nrOfCarTrips >= 1, "usedCar", "notUsedCar")) 
 
 #count how often each case happens in the different areas
-personsNrOfCarUsers <- persons %>% count(area, usedCar)
+personsNrOfCarUsers <- persons %>% count(GN, usedCar)
 
 #data tidying
 personsNrOfCarUsers <- pivot_wider(personsNrOfCarUsers, names_from = usedCar, values_from = n)
