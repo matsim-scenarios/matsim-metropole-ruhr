9d42770edeeeab60edeb6baf28ba8513accc0b6c
GregorRyb <rybczak@vsp.tu-berlin.de>
2022-12-24 01:24:18 +0100

    delete path from r scripts

--- src/main/R/regionalModalSplit.R
+++ src/main/R/regionalModalSplit.R
@@ -7,21 +7,21 @@ library(matsim)
 library(stringr)
 library("xlsx")
 
-completeTripsTable <-readTripsTable("/Users/gregorr/Documents/work/respos/runs-svn/rvr-ruhrgebiet/v1.2.1/036/036.output_trips.csv.gz")
-person2home <- read_csv2("/Users/gregorr/Documents/work/respos/runs-svn/rvr-ruhrgebiet/v1.2.1/036/person2Home.csv")
+completeTripsTable <-readTripsTable("output_trips.csv.gz")
+person2home <- read_csv2("person2Home.csv")
 completeTripsTable <- left_join(completeTripsTable, person2home, by = "person")
 
 ##calculating regional ModalSplits
 regionalModalSplits <- completeTripsTable %>% group_by(area) %>% count(main_mode) %>%
   mutate(n = n / sum(n) * 100)
 regionalModalSplits <- pivot_wider(regionalModalSplits, names_from = main_mode, values_from = n)
-regionalModalSplits <-mutate(regionalModalSplits, car = car + ride, ride=NULL)
+regionalModalSplits <-mutate(regionalModalSplits, car = car + ride, ride = NULL)
 
 ## rvr Data
-rvrModalSplitData <- read_csv2("/Users/gregorr/Documents/work/respos/shared-svn/projects/rvr-metropole-ruhr/data/regionaleModalSplits/regionalModalSplitsRVR.csv")
+rvrModalSplitData <- read_csv2("regionalModalSplitsRVR.csv")
 modalSplitsOfModelAndRVR <- left_join(regionalModalSplits, rvrModalSplitData, by = "area")
 
-write_excel_csv2(regionalModalSplits, file = "/Users/gregorr/Documents/work/respos/shared-svn/projects/rvr-metropole-ruhr/data/regionaleModalSplits/regionalModalSplitsModelResults.csv")
+write_excel_csv2(regionalModalSplits, file = "regionalModalSplitsModelResults.csv")
 
 ########plotting
 ##### https://stackoverflow.com/questions/57107721/how-to-name-the-list-of-the-group-split-output-in-dplyr
@@ -38,7 +38,7 @@ for (ii in 1:length(completeTripsTableList)) {
 }
 
 for(i in 1:length(myplots)) {
-  png(paste("/Users/gregorr/Desktop/Plots/ModalSplit", i, ".png"), res=200)
+  png(paste("Plots/ModalSplit", i, ".png"), res=200)
   set.seed(i)
   plot(myplots[[i]]) 
   dev.off()
