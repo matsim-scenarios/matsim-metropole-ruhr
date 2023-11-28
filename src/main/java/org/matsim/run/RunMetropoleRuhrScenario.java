0fec68f28acb3006a2de43ba45d5eaee192c3e0e
Kai Nagel <nagel@vsp.tu-berlin.de>
2023-11-28 20:30:28 +0100

    clean up the args thing in a way it is hopefully ok

--- src/main/java/org/matsim/run/RunMetropoleRuhrScenario.java
+++ src/main/java/org/matsim/run/RunMetropoleRuhrScenario.java
@@ -120,16 +120,8 @@ public class RunMetropoleRuhrScenario extends MATSimApplication {
 	}
 
 	public static void main(String[] args) {
-		if ( args==null || args.length==0 || args[0]==null || args[0].isEmpty() ){
-			args = new String[]{
-					"--config:controler.lastIteration", "0"
-//				, "--download-input"
-					, "--config:plans.inputPlansFile=../../../../../../shared-svn/projects/matsim-metropole-ruhr/metropole-ruhr-v1.0/input/metropole-ruhr-v1.4-3pct.plans.xml.gz"
-//				,"--config:network.inputNetworkFile=../../../../../../public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v1.0/input/metropole-ruhr-v1.4.network_resolutionHigh-with-pt.xml.gz"
-					, "--1pct"
-					, "run"
-			};
-		}
+		// (a (presumably crappy) way to give args from java instead of from the command line See KNRunMetropoleRuhrScenario)
+
 		MATSimApplication.run(RunMetropoleRuhrScenario.class, args);
 	}
 
