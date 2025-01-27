4d8371b04f0c21461bc875239230eba060ee23f0
GregorRyb <rybczak@vsp.tu-berlin.de>
2025-01-27 17:25:27 +0100

    fix compilation error

--- src/main/java/org/matsim/prepare/CreateCommercialDemand.java
+++ src/main/java/org/matsim/prepare/CreateCommercialDemand.java
@@ -237,7 +237,7 @@ public class CreateCommercialDemand implements MATSimAppCommand {
 			log.warn("Small-scale Commercial demand already exists. Skipping generation.");
 		} else {
 			//TODO check: Wo wird das Volumen der existierenden Modelle von den erzeugten Potentialen abgezogen?
-			new GenerateSmallScaleCommercialTrafficDemand(integrateExistingTrafficToSmallScaleCommercial, null).execute(
+			new GenerateSmallScaleCommercialTrafficDemand().execute(
 				configPath.toString(),
 				"--pathToDataDistributionToZones", pathDataDistributionFile.toString(),
 				"--pathToCommercialFacilities", configPath.getParent().relativize(pathCommercialFacilities).toString(),
