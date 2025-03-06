44d193d2aa17ed2616f8886f001efa69ad5462c6
vsp-gleich <leich@vsp.tu-berlin.de>
2025-03-06 19:05:39 +0100

    save file paths

--- src/main/java/org/matsim/prepare/DeleteRoutesFromPlans.java
+++ src/main/java/org/matsim/prepare/DeleteRoutesFromPlans.java
@@ -8,11 +8,16 @@ public class DeleteRoutesFromPlans {
 
     public static void main(String[] args) throws Exception {
         String[] argsForRemoveRoutesFromPlans = new String[]{
-                "--plans=../../../public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2.0/input/metropole-ruhr-v2.0-3pct.plans-commercial.xml.gz",
-                "--output=../../../public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2.0/input/no-routes/metropole-ruhr-v2.0-3pct.plans-commercial.xml.gz",
+			//			"--input=/Users/gleich/git/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2024/metropole-ruhr-v2024.0/input/metropole-ruhr-v2024.0-3pct.plans.xml.gz",
+//			"--output=/Users/gleich/git/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2024/metropole-ruhr-v2024.0/input/metropole-ruhr-v2024.0-3pct.plans-fixed.xml.gz",
+			"--plans=/Users/gleich/git/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2024/metropole-ruhr-v2024.0/input/metropole-ruhr-v2024.0-3pct.plans.xml.gz",
+			"--output=/Users/gleich/git/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2024/metropole-ruhr-v2024.0/input/metropole-ruhr-v2024.0-3pct.plans-no-routes-keep-activity-locations.xml.gz",
+
+//			"--plans=/Users/gleich/git/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2024/metropole-ruhr-v2024.0/input/metropole-ruhr-v2024.0-3pct.plans-fixedSubtours.xml.gz",
+//                "--output=/Users/gleich/git/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2024/metropole-ruhr-v2024.0/input/metropole-ruhr-v2024.0-3pct.plans-fixedSubtours-no-routes.xml.gz",
                 "--remove-routes=true",
                 "--remove-unselected-plans=true",
-                "--remove-activity-location=true"
+                "--remove-activity-location=false"
         };
         new CleanPopulation().execute(argsForRemoveRoutesFromPlans);
     }
--- src/main/java/org/matsim/prepare/FixSubTourModesInPlans.java
+++ src/main/java/org/matsim/prepare/FixSubTourModesInPlans.java
@@ -6,8 +6,13 @@ public class FixSubTourModesInPlans {
 
     public static void main(String[] args) throws Exception {
         String[] argsFixSubToursInPlans = new String[]{
-                "--input=plans.xml.gz",
-                "--output=plans-fixed.xml.gz",
+			"--input=/Users/gleich/git/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2024/metropole-ruhr-v2024.0/input/metropole-ruhr-v2024.0-3pct.plans-no-routes.xml.gz",
+			"--output=/Users/gleich/git/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2024/metropole-ruhr-v2024.0/input/metropole-ruhr-v2024.0-3pct.plans-no-routes-fixedSubtours.xml.gz",
+
+//			"--input=/Users/gleich/git/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2024/metropole-ruhr-v2024.0/input/metropole-ruhr-v2024.0-3pct.plans.xml.gz",
+//			"--output=/Users/gleich/git/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v2024/metropole-ruhr-v2024.0/input/metropole-ruhr-v2024.0-3pct.plans-fixedSubtours.xml.gz",
+//			"--input=/Users/gleich/Projekte/RVR/RVR_Run_2025-02-14/input_metropole-ruhr-v2024.0-without_mobimp/metropole-ruhr-v2024.0-3pct.output_plans_without_routes_and_facilities.xml.gz",
+//                "--output=/Users/gleich/Projekte/RVR/RVR_Run_2025-02-14/input_metropole-ruhr-v2024.0-without_mobimp/metropole-ruhr-v2024.0-3pct.output_plans_without_routes_and_facilities-fixedSubtours.xml.gz",
                 "--all-plans",
                 "--mass-conservation"
         };
