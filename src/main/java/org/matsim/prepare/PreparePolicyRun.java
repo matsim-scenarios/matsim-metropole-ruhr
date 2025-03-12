9b4241082024e56ccdfd14f36bd9f91476ad660d
rakow <rakow@vsp.tu-berlin.de>
2025-03-12 10:57:01 +0100

    update prepare population script

--- src/main/java/org/matsim/prepare/PreparePolicyRun.java
+++ src/main/java/org/matsim/prepare/PreparePolicyRun.java
@@ -1,7 +1,6 @@
 package org.matsim.prepare;
 
 import org.matsim.application.prepare.population.FixSubtourModes;
-import org.matsim.prepare.clean.CleanFacilities;
 import org.matsim.prepare.clean.PreparePopulationForPolicy;
 
 /**
@@ -15,26 +14,12 @@ public class PreparePolicyRun {
 		// The updated network
 		String network = "<path to network file>";
 
-		// The updated transit schedule
-		String transitSchedule = "<path to transit schedule file>";
-
 		// Calibrated population
 		String population = "<path to population file>";
-
-		// Generated facilities from base case
-		String facilities = "<path to facilities file>";
-
-		new CleanFacilities().execute(
-			"--network", network,
-			"--facilities", facilities,
-			"--output", "cleaned-facilities.xml.gz"
-		);
 
 		new PreparePopulationForPolicy().execute(
 			"--population", population,
 			"--network", network,
-			"--facilities", "cleaned-facilities.xml.gz",
-			"--transit-schedule", transitSchedule,
 
 			// These two files need to be used in the policy run
 			"--output-population", "prepared-population.xml.gz",
--- src/main/java/org/matsim/prepare/clean/PreparePopulationForPolicy.java
+++ src/main/java/org/matsim/prepare/clean/PreparePopulationForPolicy.java
@@ -1,7 +1,7 @@
 package org.matsim.prepare.clean;
 
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.*;
 import org.matsim.application.MATSimAppCommand;
@@ -12,18 +12,15 @@ import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.population.algorithms.ParallelPersonAlgorithmUtils;
 import org.matsim.core.population.algorithms.PersonAlgorithm;
+import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.facilities.ActivityFacilities;
 import org.matsim.facilities.FacilitiesFromPopulation;
 import org.matsim.facilities.FacilitiesWriter;
-import org.matsim.pt.routes.DefaultTransitPassengerRoute;
-import org.matsim.pt.transitSchedule.api.TransitLine;
-import org.matsim.pt.transitSchedule.api.TransitRoute;
-import org.matsim.pt.transitSchedule.api.TransitSchedule;
-import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
 import picocli.CommandLine;
 
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
 
 @CommandLine.Command(
 		name = "prepare-population-for-policy",
@@ -38,12 +35,6 @@ public class PreparePopulationForPolicy implements MATSimAppCommand {
 
 	@CommandLine.Option(names = "--network", description = "Path to network", required = true)
 	private String networkPath;
-
-	@CommandLine.Option(names = "--facilities", description = "Path to facilities", required = true)
-	private String facilitiesPath;
-
-	@CommandLine.Option(names = "--transit-schedule", description = "Path to transit schedule", required = true)
-	private String transitSchedulePath;
 
 	@CommandLine.Option(names = "--output-population", description = "Path to output population", required = true)
 	private Path outputPopulationPath;
@@ -63,14 +54,11 @@ public class PreparePopulationForPolicy implements MATSimAppCommand {
 		Population population = PopulationUtils.readPopulation(populationPath);
 		Network network = NetworkUtils.readNetwork(networkPath);
 
-		new TransitScheduleReader(scenario).readFile(transitSchedulePath);
-
 		// Delete link ids, that are now invalid
 		ParallelPersonAlgorithmUtils.run(population, Runtime.getRuntime().availableProcessors(), PersonNetworkLinkCheck.createPersonAlgorithm(network));
 
-		// Delete activity facilities that are not present (in cleaned facilities)
-		// Delete PT routes with transit routes or transit lines that are not present in the transitSchedule
-		ParallelPersonAlgorithmUtils.run(population, Runtime.getRuntime().availableProcessors(), new Cleaner(scenario.getActivityFacilities(), scenario.getTransitSchedule()));
+		// Clean facilities and old pt routes
+		ParallelPersonAlgorithmUtils.run(population, Runtime.getRuntime().availableProcessors(), new Cleaner());
 
 		// Create the missing facilities once like in PrepareSim
 		scenario.getConfig().facilities().setFacilitiesSource(FacilitiesConfigGroup.FacilitiesSource.onePerActivityLinkInPlansFile);
@@ -79,6 +67,8 @@ public class PreparePopulationForPolicy implements MATSimAppCommand {
 		f.setAssignLinksToFacilitiesIfMissing(network);
 		f.run(population);
 
+		//XY2LinksForFacilities.run(carOnlyNetwork, this.activityFacilities);
+
 		// Write the resulting population and generated facility file
 		PopulationUtils.writePopulation(population, outputPopulationPath.toString());
 		new FacilitiesWriter(scenario.getActivityFacilities()).write(outputFacilitiesPath.toString());
@@ -86,41 +76,42 @@ public class PreparePopulationForPolicy implements MATSimAppCommand {
 		return 0;
 	}
 
+	/**
+	 * Delete all facility ids and routing information of pt trips.
+	 */
 	private static final class Cleaner implements PersonAlgorithm {
-
-		private final ActivityFacilities facilities;
-		private final TransitSchedule transitSchedule;
-
-		private Cleaner(ActivityFacilities facilities, TransitSchedule transitSchedule) {
-			this.facilities = facilities;
-			this.transitSchedule = transitSchedule;
-		}
 
 		@Override
 		public void run(Person person) {
 
 			for (Plan plan : person.getPlans()) {
+
+				// Remove all pt trips
+				for (TripStructureUtils.Trip trip : TripStructureUtils.getTrips(plan)) {
+
+					boolean hasPT = trip.getLegsOnly().stream().anyMatch(l -> Objects.equals(l.getMode(), TransportMode.pt));
+
+					if (!hasPT)
+						continue;
+
+					List<PlanElement> planElements = trip.getTripElements();
+
+					// Replaces all trip elements and inserts single leg
+					final List<PlanElement> fullTrip =
+						planElements.subList(
+							planElements.indexOf(trip.getOriginActivity()) + 1,
+							planElements.indexOf(trip.getDestinationActivity()));
+
+					fullTrip.clear();
+					Leg leg = PopulationUtils.createLeg(TransportMode.pt);
+					TripStructureUtils.setRoutingMode(leg, TransportMode.pt);
+					fullTrip.add(leg);
+				}
+
+				// Delete all facility information, will be set later
 				for (PlanElement el : plan.getPlanElements()) {
 					if (el instanceof Activity act) {
-						if (act.getFacilityId() != null) {
-							if (!facilities.getFacilities().containsKey(act.getFacilityId())) {
-								act.setFacilityId(null);
-							}
-						}
-					} else if (el instanceof Leg leg) {
-
-						if (leg.getRoute() instanceof DefaultTransitPassengerRoute ptRoute) {
-							Id<TransitLine> lineId = ptRoute.getLineId();
-							if (!transitSchedule.getTransitLines().containsKey(lineId)) {
-								leg.setRoute(null);
-							} else {
-								Id<TransitRoute> routeId = ptRoute.getRouteId();
-								TransitLine line = transitSchedule.getTransitLines().get(lineId);
-								if (!line.getRoutes().containsKey(routeId)) {
-									leg.setRoute(null);
-								}
-							}
-						}
+						act.setFacilityId(null);
 					}
 				}
 			}
