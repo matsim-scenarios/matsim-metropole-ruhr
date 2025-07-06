8de4ff86c70bae926cd55d22e892665b536ad11f
GregorRyb <rybczak@vsp.tu-berlin.de>
2025-07-06 16:28:51 +0200

    pass events correctly

--- src/main/java/org/matsim/run/BusPunishmentEventHandler.java
+++ src/main/java/org/matsim/run/BusPunishmentEventHandler.java
@@ -1,5 +1,6 @@
 package org.matsim.run;
 
+import com.google.inject.Inject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.Id;
@@ -7,6 +8,7 @@ import org.matsim.api.core.v01.events.PersonEntersVehicleEvent;
 import org.matsim.api.core.v01.events.PersonMoneyEvent;
 import org.matsim.api.core.v01.events.PersonScoreEvent;
 import org.matsim.api.core.v01.events.handler.PersonEntersVehicleEventHandler;
+import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.vehicles.Vehicle;
 
 import java.util.List;
@@ -23,7 +25,8 @@ public class BusPunishmentEventHandler implements PersonEntersVehicleEventHandle
 	private static final Logger log = LogManager.getLogger(BusPunishmentEventHandler.class);
 	private static final AtomicInteger busLogCount = new AtomicInteger(0);
 
-
+	@Inject
+	EventsManager eventsManager;
 
 	BusPunishmentEventHandler(List<Id<Vehicle>> buses) {
 		this.buses = buses;
@@ -44,9 +47,12 @@ public class BusPunishmentEventHandler implements PersonEntersVehicleEventHandle
 				//TODO -100 is a placeholder, adjust the amount as needed, need to find a better value
 				//might be worth considering to make this somehow distance based or time dependent as for short trips the penalty might be too high and there are
 				//also some people that can only the pt when it is a bus?
-				new PersonMoneyEvent(event.getTime(), event.getPersonId(), -100.0, "punishment_for_using_bus", null, null);
+				PersonMoneyEvent personMoneyEvent= new PersonMoneyEvent(event.getTime(), event.getPersonId(), -100.0, "punishment_for_using_bus", null, null);
 				//person score event
-				new PersonScoreEvent(event.getTime(), event.getPersonId(), -100.0, "punishment_for_using_bus");
+				PersonScoreEvent personScoreEvent =  new PersonScoreEvent(event.getTime(), event.getPersonId(), -100.0, "punishment_for_using_bus");
+				//fire the events
+				eventsManager.processEvent(personMoneyEvent);
+				eventsManager.processEvent(personScoreEvent);
 			}
 		}
 	}
