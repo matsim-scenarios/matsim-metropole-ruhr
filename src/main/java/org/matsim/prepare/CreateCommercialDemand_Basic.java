dffe0d0d0b3e02ebaccfae36ac5bdef23ac3f4cc
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2026-06-16 13:54:56 +0200

    change default

--- src/main/java/org/matsim/prepare/CreateCommercialDemand_Basic.java
+++ src/main/java/org/matsim/prepare/CreateCommercialDemand_Basic.java
@@ -110,7 +110,7 @@ public class CreateCommercialDemand_Basic implements MATSimAppCommand {
 	@CommandLine.Option(names = "--nameOfExistingCarriersSmallScaleCommercial", description = "Path to the existing carriers file")
 	private String nameOfExistingCarriersSmallScaleCommercial;
 
-	@CommandLine.Option(names = "--additionalTravelBufferPerIterationInMinutes", description = "Additional buffer for the travel time", defaultValue = "60")
+	@CommandLine.Option(names = "--additionalTravelBufferPerIterationInMinutes", description = "Additional buffer for the travel time", defaultValue = "120")
 	private int additionalTravelBufferPerIterationInMinutes;
 
 	@CommandLine.Option(names = "--factorForTravelBufferCalculation", description = "The factor describing how many vehicles should be created in relation to the number of created services (for small-scale-commercial). If maxNumberOfLoopsForVRPSolving > 0 more vehicles are added in the replanning process.", defaultValue = "1.1")
