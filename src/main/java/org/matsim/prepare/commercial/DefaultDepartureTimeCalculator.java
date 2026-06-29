72626e83d34cb7974882b415199cb12a27ef10ce
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2026-06-29 12:19:08 +0200

    update time calculation

--- src/main/java/org/matsim/prepare/commercial/DefaultDepartureTimeCalculator.java
+++ src/main/java/org/matsim/prepare/commercial/DefaultDepartureTimeCalculator.java
@@ -58,20 +58,31 @@ public class DefaultDepartureTimeCalculator implements DepartureTimeCalculator {
 	}
 
     @Override
-    public double calculateDepartureTime(Person freightDemandDataRelation) {
-        if (CommercialTrafficUtils.getGoodsType(freightDemandDataRelation) == 140) // waste collection
-            return rnd.nextInt(6 * 3600, (7 * 3600));
-        if (CommercialTrafficUtils.getGoodsType(freightDemandDataRelation) == 150) // parcel delivery
-            if (CommercialTrafficUtils.getParcelOperator(freightDemandDataRelation).equals("dhl"))
-                return rnd.nextInt((int) (9.5 * 3600), 11 * 3600);
-            else if (CommercialTrafficUtils.getParcelOperator(freightDemandDataRelation).equals("dpd"))
-                return rnd.nextInt(8 * 3600, (int) (10.25 * 3600));
-            else if (CommercialTrafficUtils.getParcelOperator(freightDemandDataRelation).equals("ups"))
-                return rnd.nextInt(8 * 3600, (int) (9.25 * 3600));
-            else if (CommercialTrafficUtils.getParcelOperator(freightDemandDataRelation).equals("hermes"))
-                return rnd.nextInt(8 * 3600, 12 * 3600);
-            else if (CommercialTrafficUtils.getParcelOperator(freightDemandDataRelation).equals("gls"))
-                return rnd.nextInt(8 * 3600, 10 * 3600);
+	public double calculateDepartureTime(Person freightDemandDataRelation) {
+		if (CommercialTrafficUtils.getGoodsType(freightDemandDataRelation) == 140) // waste collection
+			return rnd.nextInt(6 * 3600, (7 * 3600));
+		else if (CommercialTrafficUtils.getGoodsType(freightDemandDataRelation) == 150) // parcel delivery
+			if (CommercialTrafficUtils.getParcelOperator(freightDemandDataRelation).equals("dhl"))
+				return rnd.nextInt((int) (9.5 * 3600), 11 * 3600);
+			else if (CommercialTrafficUtils.getParcelOperator(freightDemandDataRelation).equals("dpd"))
+				return rnd.nextInt(8 * 3600, (int) (10.25 * 3600));
+			else if (CommercialTrafficUtils.getParcelOperator(freightDemandDataRelation).equals("ups"))
+				return rnd.nextInt(8 * 3600, (int) (9.25 * 3600));
+			else if (CommercialTrafficUtils.getParcelOperator(freightDemandDataRelation).equals("hermes"))
+				return rnd.nextInt(8 * 3600, 12 * 3600);
+			else if (CommercialTrafficUtils.getParcelOperator(freightDemandDataRelation).equals("gls"))
+				return rnd.nextInt(8 * 3600, 10 * 3600);
+			else
+				throw new RuntimeException("Parcel operator not recognized: " + CommercialTrafficUtils.getParcelOperator(freightDemandDataRelation));
+		else if (CommercialTrafficUtils.getTransportType(freightDemandDataRelation).contains("FTL"))
+			return calculateLongDistanceDepartureTime();
+		else if (CommercialTrafficUtils.getTransportType(freightDemandDataRelation).equals("LTL"))
+			return rnd.nextInt(6 * 3600, 12 * 3600);
+		else
+			throw new RuntimeException("Transport type not recognized: " + CommercialTrafficUtils.getTransportType(freightDemandDataRelation));
+	}
+
+	private int calculateLongDistanceDepartureTime() {
 		DurationsBounds durationBounds = longDistanceStartTimeDistribution.sample();
 		return rnd.nextInt(durationBounds.lowerBoundStartTime * 3600, durationBounds.upperBoundStartTime * 3600);
     }
